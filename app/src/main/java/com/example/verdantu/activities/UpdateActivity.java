8473224c7f16d71d615ae94ffe33a0a3188adc76
Santhosh <santhosh.karthik@outlook.com>
2020-05-13 12:19:48 +1000

    Update moved to activities foler

--- app/src/main/java/com/example/verdantu/activities/UpdateActivity.java
+++ app/src/main/java/com/example/verdantu/activities/UpdateActivity.java
@@ -1,10 +1,11 @@
-package com.example.verdantu;
+package com.example.verdantu.activities;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.verdantu.R;
 import com.google.android.material.tabs.TabLayout;
 
 public class UpdateActivity extends AppCompatActivity {
